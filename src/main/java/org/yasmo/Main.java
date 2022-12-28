1cb4b10b8b45db203f7ca02ad1f77a72a20a1ea1
YATAHAKI <pe4kayt@gmail.com>
2022-12-28 19:35:33 +0300

    <UPDATE> DataBase connection.

--- src/main/java/org/yasmo/Main.java
+++ src/main/java/org/yasmo/Main.java
@@ -1,7 +1,19 @@
 package org.yasmo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws SQLException {
+        String ConnUrl = "jdbc:mysql://localhost:3306/DataBaseReg";
+        String Login = "root";
+        String Pass = "ToAAYoJKR2";
+
+        try(Connection ConnDb = DriverManager.getConnection(ConnUrl, Login, Pass)) {
+            System.out.println("DB connected");
+    }
+
+
     }
 }
